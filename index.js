01bf63aa32d43fafcb334f61f341edde21fcd6d7
Eliot Scott <eliotvscott@gmail.com>
2018-04-16 15:01:53 -0300

    Chat app with responsive bot -- Bootstrapped with Botmaster Socket.io

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express"); //added
+const port = process.env.PORT || 3000; //added
+const app = express(); //added
+
+// Routing for index.html
+app.use(express.static(__dirname + "/public")); //added
+
+const server = app.listen(port, "0.0.0.0", () => {
+	//added
+	console.log("Server listening at port %d", port);
+});
+
+const Botmaster = require("botmaster");
+const SocketioBot = require("botmaster-socket.io");
+
+const botmaster = new Botmaster({
+	server
+});
+
+const socketioSettings = {
+	id: "SOME_BOT_ID_OF_YOUR_CHOOSING",
+	server
+};
+
+const socketioBot = new SocketioBot(socketioSettings);
+botmaster.addBot(socketioBot);
+
+botmaster.use({
+	type: "incoming",
+	name: "my-middleware",
+	controller: (bot, update) => {
+		return bot.reply(update, "Hello world!");
+	}
+});
+
+botmaster.on("error", (bot, err) => {
+	// added
+	console.log(err.stack); // added
+}); // added
